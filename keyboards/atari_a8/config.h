8ca4ab5b10d75632e05c88796eb084833b220327
Jeremy J Starcher <jeremy.starcher@gmail.com>
2023-12-03 01:04:04 -0500

    Replaced the OLED with HD44780
    Need to find the right pins

--- keyboards/atari_a8/config.h
+++ keyboards/atari_a8/config.h
@@ -27,3 +27,24 @@
 
 #define JOYSTICK_AXIS_COUNT 2
 #define JOYSTICK_BUTTON_COUNT 1
+
+// An array of four GPIO pins connected to the display's D4-D7 pins
+#define HD44780_DATA_PINS 	{ F1, F3, F2, F6 }
+
+// (Required) The GPIO connected to the display's RS pin
+#define HD44780_RS_PIN 	B1
+
+// (Required) The GPIO connected to the display's RW pin
+#define HD44780_RW_PIN 	B0
+
+// (Required) The GPIO connected to the display's E pin
+#define HD44780_E_PIN 	B2
+
+// The number of visible characters on a single line of the display
+// #define HD44780_DISPLAY_COLS 	16
+
+// The number of visible lines on the display
+// #define HD44780_DISPLAY_LINES 	2
+
+// 	If defined, input characters will wrap to the next line
+// #define HD44780_WRAP_LINES
--- keyboards/atari_a8/rules.mk
+++ keyboards/atari_a8/rules.mk
@@ -1,6 +1,10 @@
-OLED_ENABLE = yes
+OLED_ENABLE = no
 OLED_DRIVER = SSD1306
 OLED_TRANSPORT = i2c
 OLED_DISPLAY_ADDRESS =	0x3C
 
 JOYSTICK_ENABLE = yes
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_hd44780.md
+HD44780_ENABLE = yes
+
