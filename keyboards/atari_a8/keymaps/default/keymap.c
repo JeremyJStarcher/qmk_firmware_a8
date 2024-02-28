f1b1c3635bc0c75076d49b9e186019685b8a4770
Jeremy J Starcher <jeremy.starcher@gmail.com>
2024-02-27 19:08:46 -0500

    Moved the atari800_emu stuff to its own function

--- keyboards/atari_a8/keymaps/default/keymap.c
+++ keyboards/atari_a8/keymaps/default/keymap.c
@@ -1,12 +1,9 @@
 #include QMK_KEYBOARD_H
 
-// #include <util/delay.h>
-
 static void render_logo(void);
-
 bool render_frame(void);
-
 bool run_max = false;
+bool process_record_atari800_emu(uint16_t keycode, keyrecord_t *record);
 
 /*
     Known issue: I can't find any way to send a CTRL-< (CLEAR) key code.
@@ -252,18 +249,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 #endif
 
-    switch (keycode) {
-            // case KC_CCCV:  // One key copy/paste
-            //     if (record->event.pressed) {
-            //         copy_paste_timer = timer_read();
-            //     } else {
-            //         if (timer_elapsed(copy_paste_timer) > TAPPING_TERM) {  // Hold, copy
-            //             tap_code_delay16(LCTL(KC_C));
-            //         } else {  // Tap, paste
-            //             tap_code_delay16(LCTL(KC_V));
-            //         }
-            //     } return true;
+    return process_record_atari800_emu(keycode, record);
 
+}
+
+
+bool process_record_atari800_emu(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
         case AT_MENU:
             handle_key(KC_F1, record->event.pressed);
             return true;
@@ -303,6 +295,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true;
     }
 }
+
+
 
 static void render_logo(void) {
     static const char PROGMEM qmk_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00};
